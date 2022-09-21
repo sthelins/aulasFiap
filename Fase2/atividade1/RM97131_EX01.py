f564826c970dd4926c0ceaf6153f9a09ba542da3
Sthefany <sthefanylins_albuquerque@hotmail.com>
2022-09-21 16:56:22 -0300

    exercício 1 e 2 da ativ1

---
+++ Fase2/atividade1/RM97131_EX01.py
@@ -0,0 +1,32 @@
+#entrada - assinatura do cliente, faturamento anual
+#calcular valor do bonus
+#saída - o valor do bonus e o que o cliente deve pagar
+
+#BASIC - 30%
+#SILVER - 20%
+#GOLD - 10%
+#PLATINUM - 5%
+
+print("Digite o número de acordo com o sua assinatura")
+assinatura = int(input("1 - BASIC| 2 - SILVER| 3 - GOLD | 4 - PLATINUM  "))
+faturamento = float(input("Agora, digite seu faturamento anual  "))
+pagamento = 0
+bonus = 0
+
+if assinatura == 1:
+    pagamento = faturamento * 0.3
+    bonus = 30
+elif assinatura == 2:
+    pagamento = faturamento * 0.2
+    bonus = 20
+elif assinatura == 3:
+    pagamento = faturamento * 0.1
+    bonus = 10
+elif pagamento == 4:
+    pagamento = faturamento * 0.05
+    bonus = 5
+else:
+    print("Plano não existente")
+
+print(f"O bônus é {bonus}% e o que deve ser pago é R${pagamento}")
+
---
+++ Fase2/atividade1/RM97131_EX02.py
@@ -0,0 +1,23 @@
+#entrada - quantidade de votos de cada dia da semana
+#calculo - qual o mais votado
+#saída - o mais votado
+
+print("Informe o número de votos que cada dia recebeu")
+segunda = int(input("Segunda "))
+terca = int(input("Terça "))
+quarta = int(input("Quarta "))
+quinta = int(input("Quinta "))
+sexta = int(input("Sexta "))
+
+if segunda > terca and segunda > quarta and segunda > quinta and segunda > sexta:
+    print("O vencedor foi Segunda-feira")
+elif terca > segunda and terca > quarta and terca > quinta and terca > sexta:
+    print("O vencedor foi Terça-feira")
+elif quarta > segunda and quarta > terca and quarta > quinta and quarta > sexta:
+    print("O vencedor foi Quarta-feira")
+elif quinta > segunda and quinta > terca and quinta > quarta and quinta > sexta:
+    print("O vencedor foi Quinta-feira")
+elif sexta > segunda and sexta > terca and sexta > quarta and sexta > quinta:
+    print("O vencedor foi Sexta-feira")
+else:
+    print("O programa não trabalha com empates, por favor refaça a votação")
